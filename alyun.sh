833f9679a516a07a580790158a02b87b072049b3
huxuyang <huxuyang0038@163.com>
2025-12-25 15:35:53 +0800

    v0.11

--- alyun.sh
+++ alyun.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
-#
-#********************************************************************
-#Author:            wangxiaochun
-#QQ:                29308620
-#Date:              2025-12-04
-#FileName:          alyun.sh
-#URL:               http://www.wangxiaochun.com
-#Description:       The test script
-#Copyright (C):     2025 All rights reserved
-#********************************************************************
 rm -rf /etc/apt/sources.list.d/*
 sudo sed -i \
   -e 's#http://mirrors.ustc.edu.cn/ubuntu/#http://mirrors.aliyun.com/ubuntu/#g' \
